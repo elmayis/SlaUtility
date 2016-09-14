2636183308d8fa667a1fe1a2bc83139813ddd089
elmayis <marhtrev@gmail.com>
2016-09-14 07:56:32 -0500

    Initial commit
    Created the application using VS2015

---
+++ SlaUtility/SlaUtility.h
@@ -0,0 +1,32 @@
+
+// SlaUtility.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CSlaUtilityApp:
+// See SlaUtility.cpp for the implementation of this class
+//
+
+class CSlaUtilityApp : public CWinApp
+{
+public:
+	CSlaUtilityApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSlaUtilityApp theApp;
---
+++ SlaUtility/SlaUtilityDlg.h
@@ -0,0 +1,34 @@
+
+// SlaUtilityDlg.h : header file
+//
+
+#pragma once
+
+
+// CSlaUtilityDlg dialog
+class CSlaUtilityDlg : public CDialogEx
+{
+// Construction
+public:
+	CSlaUtilityDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_SLAUTILITY_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
