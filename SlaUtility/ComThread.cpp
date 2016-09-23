981421303f59c009239fe9e0b4ec7487f56c2821
elmayis <marhtrev@gmail.com>
2016-09-23 13:32:35 -0500

    COM thread
    Added COM thread class based on CWinThread

---
+++ SlaUtility/ComThread.cpp
@@ -0,0 +1,29 @@
+
+#include "stdafx.h"
+#include "ComThread.h"
+
+IMPLEMENT_DYNCREATE(CComThread, CWinThread)
+
+CComThread::CComThread()
+{
+
+}
+
+CComThread::~CComThread()
+{
+
+}
+
+BEGIN_MESSAGE_MAP(CComThread, CWinThread)
+END_MESSAGE_MAP()
+
+BOOL CComThread::InitInstance()
+{
+   return TRUE;
+}
+
+int CComThread::ExitInstance()
+{
+   return 0;
+}
+
---
+++ SlaUtility/ComThread.h
@@ -0,0 +1,26 @@
+#pragma once
+
+/**
+*/
+class CComThread : public CWinThread
+{
+public:
+   DECLARE_DYNCREATE(CComThread)
+
+   CComThread();
+   virtual ~CComThread();
+
+   /**
+      Posts message on thread to write to the buffer to the COM port
+   */
+   void FireWriteBuffer();
+
+protected:
+   virtual BOOL InitInstance();
+   virtual int ExitInstance();
+
+   DECLARE_MESSAGE_MAP()
+
+private:
+
+};
