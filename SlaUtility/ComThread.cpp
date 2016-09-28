647ec28672acb9b929fd3376f2f353ac7577256c
elmayis <marhtrev@gmail.com>
2016-09-28 06:35:09 -0500

    Added comments
    Comments describe passing delegates between
    thread boundaries

--- SlaUtility/ComThread.cpp
+++ SlaUtility/ComThread.cpp
@@ -18,6 +18,10 @@ CComThread::~CComThread()
 
 void CComThread::FireConnect(const ConnectFinishedDelegate& oConnectFinishedDelegate)
 {
+   // Allocate a copy of the delegate on the heap. This needs to be done so that the raw pointer can be
+   // passed to this thread across thread bounderies. The OS layer is only capable of dealing with simple POD.
+   // The pointer will be free'd by the OnConnect message handler.
+   //
    ConnectFinishedDelegate* poDispatch = new ConnectFinishedDelegate(oConnectFinishedDelegate);
    PostThreadMessage(WM_CONNECT, reinterpret_cast<WPARAM>(poDispatch), 0);
 }
@@ -44,6 +48,9 @@ int CComThread::ExitInstance()
 
 void CComThread::OnConnect(WPARAM wParam, LPARAM lParam)
 {
+   // Cast in the ConnectFinishedDelegate handle. It is required that this thing be on the heap. Wrap it in a shared_ptr
+   // so that it is destroyed upon function exit.
+   //
    std::shared_ptr<ConnectFinishedDelegate> spoDispatch(reinterpret_cast<ConnectFinishedDelegate*>(wParam));
    OnConnectFinished = *spoDispatch;
    OnConnectFinished(-1);
