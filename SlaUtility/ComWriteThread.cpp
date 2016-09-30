fb4f04f0e2785f3dab46c0a22685f4f1ee2fb872
elmayis <marhtrev@gmail.com>
2016-09-30 06:44:06 -0500

    COM Read/write thread
    Initial addition of COM read and write threads. The classes were copied
    from CComThread.

---
+++ SlaUtility/ComWriteThread.cpp
@@ -0,0 +1,163 @@
+
+#include "stdafx.h"
+#include "StatusCodes.h"
+#include "ComWriteThread.h"
+
+IMPLEMENT_DYNCREATE(CComWriteThread, CWinThread)
+
+CComWriteThread::CComWriteThread()
+:  m_hComm(NULL)
+{
+}
+
+CComWriteThread::~CComWriteThread()
+{
+   if (NULL != m_hComm)
+   {
+      CloseHandle(m_hComm);
+      m_hComm = NULL;
+   }
+}
+
+void CComWriteThread::SetOutputMsgDelegate(const OutputMsgDelegate& oOutputMsgDelegate)
+{
+   OnOutputMsg = oOutputMsgDelegate;
+}
+
+void CComWriteThread::FireConnect(const ConnectFinishedDelegate& oConnectFinishedDelegate, const CComSettings& oComSettings)
+{
+   // Allocate a copy of the delegate on the heap. This needs to be done so that the raw pointer can be
+   // passed to this thread across thread bounderies. The OS layer is only capable of dealing with simple POD.
+   // The pointer will be free'd by the OnConnect message handler.
+   //
+   ConnectFinishedDelegate* poDispatch = new ConnectFinishedDelegate(oConnectFinishedDelegate);
+   CComSettings* poSettings = new CComSettings(oComSettings);
+   PostThreadMessage(WM_CONNECT, reinterpret_cast<WPARAM>(poDispatch), reinterpret_cast<LPARAM>(poSettings));
+}
+
+void CComWriteThread::FireWriteBuffer()
+{
+   PostThreadMessage(WM_WRITE_BUFFER, 0, 0);
+}
+
+BEGIN_MESSAGE_MAP(CComWriteThread, CWinThread)
+   ON_THREAD_MESSAGE(WM_CONNECT, OnConnect)
+   ON_THREAD_MESSAGE(WM_WRITE_BUFFER, OnWriteBuffer)
+END_MESSAGE_MAP()
+
+BOOL CComWriteThread::InitInstance()
+{
+   return TRUE;
+}
+
+int CComWriteThread::ExitInstance()
+{
+   return 0;
+}
+
+void CComWriteThread::OnConnect(WPARAM wParam, LPARAM lParam)
+{
+   // Cast in the ConnectFinishedDelegate handle. It is required that this thing be on the heap. Wrap it in a shared_ptr
+   // so that it is destroyed upon function exit.
+   //
+   std::shared_ptr<ConnectFinishedDelegate> spoDispatch(reinterpret_cast<ConnectFinishedDelegate*>(wParam));
+   m_soComSettings.reset(reinterpret_cast<CComSettings*>(lParam));
+
+   int iErrCode = CStatusCodes::SC_COM_OPEN_FAILED;
+   if (OpenComm())
+   {
+      iErrCode = CStatusCodes::SC_COM_SETTINGS_FAILED;
+      if (UpdateCommSettings())
+      {
+         iErrCode = CStatusCodes::SC_OK;
+      }
+   }
+   (*spoDispatch)(iErrCode);
+}
+
+void CComWriteThread::OnWriteBuffer(WPARAM wParam, LPARAM lParam)
+{
+
+}
+
+bool CComWriteThread::OpenComm(void)
+{
+   CString sCommPort;
+   // Need to add the text "COM" to the port number
+   //
+   sCommPort.Format("\\\\.\\COM%d", m_soComSettings->m_iPortNumber);
+   m_hComm =
+      CreateFile(sCommPort,
+         GENERIC_READ | GENERIC_WRITE,
+         0,                //  must be opened with exclusive-access
+         NULL,             //  default security attributes
+         OPEN_EXISTING,    //  must use OPEN_EXISTING
+         0,                //  not overlapped I/O
+         NULL);            //  hTemplate must be NULL for comm devices
+   if (INVALID_HANDLE_VALUE != m_hComm) return true;
+
+   CString sMsg;
+   sMsg.Format("CreateFile failed with Windows error code %d.\n", GetLastError());
+   OnOutputMsg(sMsg, true);
+   return false;
+}
+
+bool CComWriteThread::UpdateCommSettings(void)
+{
+   DCB oDcb;
+   //  Initialize the DCB structure.
+   //
+   SecureZeroMemory(&oDcb, sizeof(DCB));
+   oDcb.DCBlength = sizeof(DCB);
+   // Get the current state of the COM so that we can build upon it.
+   //
+   BOOL bSuccess = GetCommState(m_hComm, &oDcb);
+   if (bSuccess)
+   {
+      oDcb.BaudRate = m_soComSettings->m_iBaudRate;
+      oDcb.ByteSize = m_soComSettings->m_iDataBits;
+      oDcb.StopBits = m_soComSettings->m_iStopBits;
+      oDcb.Parity = m_soComSettings->m_iParity;
+      // The default is no flow control
+      //
+      oDcb.fOutxCtsFlow = false;
+      oDcb.fOutxDsrFlow = false;
+      oDcb.fRtsControl = false;
+      oDcb.fDtrControl = false;
+      oDcb.fOutX = false;
+      oDcb.fInX = false;
+      switch (m_soComSettings->m_iHandshaking)
+      {
+      case 1:
+      {
+         oDcb.fOutX = true;
+         oDcb.fInX = true;
+         break;
+      }
+      case 2:
+      {
+         oDcb.fRtsControl = true;
+         break;
+      }
+      case 3:
+      {
+         oDcb.fRtsControl = true;
+         oDcb.fOutX = true;
+         oDcb.fInX = true;
+         break;
+      }
+      }
+      if (SetCommState(m_hComm, &oDcb)) return true;
+
+      CString sMsg;
+      sMsg.Format("SetCommState failed with error %d.\n", GetLastError());
+      OnOutputMsg(sMsg, true);
+   }
+   else
+   {
+      CString sMsg;
+      sMsg.Format("GetCommState failed with error %d.\n", GetLastError());
+      OnOutputMsg(sMsg, true);
+   }
+   return false;
+}
---
+++ SlaUtility/ComWriteThread.h
@@ -0,0 +1,85 @@
+
+#pragma once
+
+#include <functional>
+#include <memory>
+#include "ComSettings.h"
+
+/**
+   Thread that reads and writes to a COM port
+*/
+class CComWriteThread : public CWinThread
+{
+public:
+   typedef std::function<void(int)> ConnectFinishedDelegate;
+   typedef std::function<void(const CString&, bool)> OutputMsgDelegate;
+
+   DECLARE_DYNCREATE(CComWriteThread)
+
+   CComWriteThread();
+   virtual ~CComWriteThread();
+
+   /**
+      Synchronous call that sets the delegate that will be called to output a message to the UI
+      @param[in] oOutputMsgDelegate delegate called to output a message to the UI
+   */
+   void SetOutputMsgDelegate(const OutputMsgDelegate& oOutputMsgDelegate);
+
+   /**
+      Asynchronous method that establishes a connection to the port selected in the given settings
+      @param[in] oConnectFinishedDelegate delegate called when the operation is finished
+      @param[in] oComSettings container with all the settings needed to establish communtion with a COM port
+   */
+   void FireConnect(const ConnectFinishedDelegate& oConnectFinishedDelegate, const CComSettings& oComSettings);
+
+   /**
+      Posts message on thread to write to the buffer to the COM port
+   */
+   void FireWriteBuffer();
+
+protected:
+   virtual BOOL InitInstance();
+   virtual int ExitInstance();
+
+   DECLARE_MESSAGE_MAP()
+
+private:
+   static const int WM_CONNECT = WM_USER + 1;
+   static const int WM_WRITE_BUFFER = WM_CONNECT + 1;
+
+   /**
+   */
+   void OnConnect(WPARAM wParam, LPARAM lParam);
+
+   /**
+   */
+   void OnWriteBuffer(WPARAM wParam, LPARAM lParam);
+
+   /**
+   Opens the COM port based on the selected port from the drop down list
+   */
+   bool OpenComm(void);
+
+   /**
+   Updates the opened COM ports settings
+
+   @return true if successfully updated the COM settings
+   */
+   bool UpdateCommSettings(void);
+
+   /**
+      Delegate called to output messages to the UI
+   */
+   OutputMsgDelegate OnOutputMsg;
+
+   /**
+      Settings passed in when requesting connection
+   */
+   std::shared_ptr<CComSettings> m_soComSettings;
+
+   /**
+   Handle to the COM port to the Arduino
+   */
+   HANDLE m_hComm;
+
+};
